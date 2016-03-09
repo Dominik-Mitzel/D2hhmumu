2a8f01d881ca2bf64cee61c0ab4cff0a30ba4ea5
Dominik Mitzel <mitzel@stud.uni-heidelberg.de>
2016-03-09 15:49:28 +0100

    promoted D2hhmumufitter to a C++ class

--- dev/src/D2hhmumuFitter.cc
+++ dev/src/D2hhmumuFitter.cc
@@ -3,21 +3,43 @@
 using namespace std;
 using namespace RooFit ;
 
-void fit_MC(){
+D2hhmumuFitter::D2hhmumuFitter():
+
+  D0_M("Dst_DTF_D0_M", "m(h h #mu #mu)", 1820., 1940.,"MeV"),
+  deltaM("deltaM","#delta m", 139.8,150,"MeV"),
+  //signal                                                                                                                                                                      
+  deltaM_xi("deltaM_xi","deltaM_xi",1.45437e+02,144,146),
+  deltaM_lambda("deltaM_lambda","deltaM_lambda",5.35039e-01,0.1,2),
+  deltaM_gamma("deltaM_gamma","deltaM_gamma",1.00164e-01,-2,2),
+  deltaM_delta("deltaM_delta","deltaM_delta",1.07829e+00,0.,10),
+  D0_M_xi("D0_M_xi","D0_M_xi",1.86560e+03,1860,1870),
+  D0_M_lambda("D0_M_lambda","D0_M_lambda",8.54839e+00,0.1,20),
+  D0_M_gamma("D0_M_gamma","D0_M_gamma",-5.42758e-02,-2,2),
+  D0_M_delta("D0_M_delta","D0_M_delta",6.09288e-01,0.,10),
+  //purely combinatorial background                                                                                                                                             
+  deltaM_threshold("deltaM_threshold","deltaM_threshold",139.57018),
+  deltaM_alpha("deltaM_alpha","deltaM_alpha",3.9761e+00,0,10.),
+  D0_M_chebyA("D0_M_chebyA","D0_M_chebyA",-3.5906e-02,-1,1),
+  D0_M_chebyB("D0_M_chebyB","D0_M_chebyB",-1.7004e-02,-1,1),
+  D0_M_chebyC("D0_M_chebyC","D0_M_chebyC",-1.7882e-02,-1,1),
+  //peaking , in mD0 just a Gaussian... to be improved!                                                                                                                         
+  mean1("mu1", "mean1", 1.8350e3,1835.,1845.),
+  sigma1("sigma_{1}", "sigma1", 1.4187e+01,3.,25.)
+
+{};
+
+D2hhmumuFitter::~D2hhmumuFitter(){}
 
 
-  gStyle->SetOptStat(0);
-  gStyle->SetTitleXSize(0.05);
-  gStyle->SetTitleYSize(0.05);
-  gStyle->SetTitleFont(42,"X");
-  gStyle->SetTitleFont(42,"Y");
-  gStyle->SetLabelFont(42,"X");
-  gStyle->SetLabelFont(42,"Y");
-  gStyle->SetLabelOffset(0.01,"X");
-  gStyle->SetPadTickX(1);
-  gStyle->SetPadTickY(1);
-  TH1::SetDefaultSumw2();
-  TH2::SetDefaultSumw2();
+void::D2hhmumuFitter::setKpimumuStartParameters(){                                                                                                                             
+                                                 
+  //later like this...                                                                                                                                                         
+  //deltaM_xi.SetValue()..
+  
+}
+
+
+void D2hhmumuFitter::fit_MC(){
 
   ///Load file                                                                                                                                                                                
   TFile* file;
@@ -30,23 +52,10 @@ void fit_MC(){
 
   ///Fill all needed variables in RooDataSet                                                                                                                                                  
   ///---------------------------------------                                                                                                                                                  
-
-  RooRealVar D0_M("Dst_DTF_D0_M", "m(h h #mu #mu)", 1800., 1940.,"MeV");
-  RooRealVar deltaM("deltaM","#delta m", 139.8,150,"MeV");
   RooArgList list =  RooArgList( D0_M,deltaM );
   RooDataSet* data = new RooDataSet("data", "data", tree, RooArgSet(D0_M,deltaM));
   RooDataSet* data_small = (RooDataSet*) data->reduce(SelectVars(RooArgSet(D0_M)));
   RooDataHist* data_binned = data_small->binnedClone();
- ///Define fit model                                                                                                                                 
-  RooRealVar deltaM_xi("deltaM_xi","deltaM_xi",1.45437e+02,144,146);
-  RooRealVar deltaM_lambda("deltaM_lambda","deltaM_lambda",5.35039e-01,0.1,2);
-  RooRealVar deltaM_gamma("deltaM_gamma","deltaM_gamma",1.00164e-01,-2,2);
-  RooRealVar deltaM_delta("deltaM_delta","deltaM_delta",1.07829e+00,0.,10);
-
-  RooRealVar D0_M_xi("D0_M_xi","D0_M_xi",1.86560e+03,1860,1870);
-  RooRealVar D0_M_lambda("D0_M_lambda","D0_M_lambda",8.54839e+00,0.1,20);
-  RooRealVar D0_M_gamma("D0_M_gamma","D0_M_gamma",-5.42758e-02,-2,2);
-  RooRealVar D0_M_delta("D0_M_delta","D0_M_delta",6.09288e-01,0.,10);
 
   D2hhmumuModel myMCModel;
   myMCModel.Signal(D0_M,deltaM,
@@ -91,20 +100,7 @@ void fit_MC(){
 }
 
 
-void fit_Data(){
-
-  gStyle->SetOptStat(0);
-  gStyle->SetTitleXSize(0.05);
-  gStyle->SetTitleYSize(0.05);
-  gStyle->SetTitleFont(42,"X");
-  gStyle->SetTitleFont(42,"Y");
-  gStyle->SetLabelFont(42,"X");
-  gStyle->SetLabelFont(42,"Y");
-  gStyle->SetLabelOffset(0.01,"X");
-  gStyle->SetPadTickX(1);
-  gStyle->SetPadTickY(1);
-  TH1::SetDefaultSumw2();
-  TH2::SetDefaultSumw2();
+void D2hhmumuFitter::fit_Data(){
 
   ///Load file                                                                                                                                                                                
   TFile* file;
@@ -115,26 +111,10 @@ void fit_Data(){
   tree->SetBranchStatus("Dst_DTF_D0_M",1);
   tree->SetBranchStatus("deltaM",1);
 
-  ///Fill all needed variables in RooDataSet                                                                                                                                                  
-  ///---------------------------------------                                                                                                                                                  
-
-  RooRealVar D0_M("Dst_DTF_D0_M", "m(h h #mu #mu)", 1820., 1940.,"MeV");
-  RooRealVar deltaM("deltaM","#delta m", 139.8,150,"MeV");
   RooArgList list =  RooArgList( D0_M,deltaM );
   RooDataSet* data = new RooDataSet("data", "data", tree, RooArgSet(D0_M,deltaM));
   RooDataSet* data_small = (RooDataSet*) data->reduce(SelectVars(RooArgSet(D0_M)));
   RooDataHist* data_binned = data_small->binnedClone();
-
-
-  RooRealVar deltaM_xi("deltaM_xi","deltaM_xi",1.45437e+02,144,146);
-  RooRealVar deltaM_lambda("deltaM_lambda","deltaM_lambda",5.35039e-01,0.1,2);
-  RooRealVar deltaM_gamma("deltaM_gamma","deltaM_gamma",1.00164e-01,-2,2);
-  RooRealVar deltaM_delta("deltaM_delta","deltaM_delta",1.07829e+00,0.,10);
-
-  RooRealVar D0_M_xi("D0_M_xi","D0_M_xi",1.86560e+03,1860,1870);
-  RooRealVar D0_M_lambda("D0_M_lambda","D0_M_lambda",8.54839e+00,0.1,20);
-  RooRealVar D0_M_gamma("D0_M_gamma","D0_M_gamma",-5.42758e-02,-2,2);
-  RooRealVar D0_M_delta("D0_M_delta","D0_M_delta",6.09288e-01,0.,10);
 
   D2hhmumuModel myModel;
   RooAbsPdf *my_signal = myModel.Signal(D0_M,deltaM,
@@ -144,15 +124,6 @@ void fit_Data(){
 
 
   //background models
-
-  //purely combinatorial
-  RooRealVar deltaM_threshold("deltaM_threshold","deltaM_threshold",139.57018);
-  RooRealVar deltaM_alpha("deltaM_alpha","deltaM_alpha",3.9761e+00,0,10.);
-
-  RooRealVar D0_M_chebyA("D0_M_chebyA","D0_M_chebyA",-3.5906e-02,-1,1);
-  RooRealVar D0_M_chebyB("D0_M_chebyB","D0_M_chebyB",-1.7004e-02,-1,1);
-  RooRealVar D0_M_chebyC("D0_M_chebyC","D0_M_chebyC",-1.7882e-02,-1,1);
-
 
   myModel.CombinatoricBackground(D0_M,deltaM,
 				 D0_M_chebyA,D0_M_chebyB,D0_M_chebyC,
@@ -166,10 +137,7 @@ void fit_Data(){
 			       deltaM_threshold,deltaM_alpha
                                );
 
-  //peaking , in mD0 jsut a Gaussian... to be improved!
-
-  RooRealVar mean1("mu1", "mean1", 1.8350e3,1835.,1845.);                                                                                             
-  RooRealVar sigma1("sigma_{1}", "sigma1", 1.4187e+01,3.,25.);                                                                                      
+  //peaking , in mD0 just a Gaussian... to be improved!
 
   myModel.D2hhhhBackground(D0_M,deltaM,
 			   mean1,sigma1,
@@ -204,7 +172,7 @@ void fit_Data(){
   myModel.GetWorkspace().pdf("D2hhmumuModel")->plotOn(frame_m,Components(*myModel.GetWorkspace().pdf("Signal")),LineColor(kRed),LineStyle(kDashed),LineWidth(1));
   myModel.GetWorkspace().pdf("D2hhmumuModel")->plotOn(frame_m,Components(*myModel.GetWorkspace().pdf("CombinatoricBkg")),LineColor(kBlue),LineStyle(kDashed),LineWidth(1));
   myModel.GetWorkspace().pdf("D2hhmumuModel")->plotOn(frame_m,Components(*myModel.GetWorkspace().pdf("RandomPionBkg")),LineColor(kCyan),LineStyle(kDashed),LineWidth(1));
-  myModel.GetWorkspace().pdf("D2hhmumuModel")->plotOn(frame_m,Components(*myModel.GetWorkspace().pdf("D2hhhhBkg")),LineColor(kYellow),LineStyle(kDashed),LineWidth(1));
+  myModel.GetWorkspace().pdf("D2hhmumuModel")->plotOn(frame_m,Components(*myModel.GetWorkspace().pdf("D2hhhhBkg")),LineColor(kGreen+3),LineStyle(kDashed),LineWidth(1));
   myModel.GetWorkspace().pdf("D2hhmumuModel")->plotOn(frame_m,Components(*myModel.GetWorkspace().pdf("D2hhhhRandomPionBkg")),LineColor(kGreen),LineStyle(kDashed),LineWidth(1));
   finalPDF->plotOn(frame_m,LineColor(kBlack),LineWidth(1)); 
   data->plotOn(frame_m,Name("data"),MarkerSize(0.5),Binning(50));
@@ -217,7 +185,7 @@ void fit_Data(){
   myModel.GetWorkspace().pdf("D2hhmumuModel")->plotOn(frame_m2,Components(*myModel.GetWorkspace().pdf("Signal")),LineColor(kRed),LineStyle(kDashed),LineWidth(1));
   myModel.GetWorkspace().pdf("D2hhmumuModel")->plotOn(frame_m2,Components(*myModel.GetWorkspace().pdf("CombinatoricBkg")),LineColor(kBlue),LineStyle(kDashed),LineWidth(1));
   myModel.GetWorkspace().pdf("D2hhmumuModel")->plotOn(frame_m2,Components(*myModel.GetWorkspace().pdf("RandomPionBkg")),LineColor(kCyan),LineStyle(kDashed),LineWidth(1));
-  myModel.GetWorkspace().pdf("D2hhmumuModel")->plotOn(frame_m2,Components(*myModel.GetWorkspace().pdf("D2hhhhBkg")),LineColor(kYellow),LineStyle(kDashed),LineWidth(1)); 
+  myModel.GetWorkspace().pdf("D2hhmumuModel")->plotOn(frame_m2,Components(*myModel.GetWorkspace().pdf("D2hhhhBkg")),LineColor(kGreen+3),LineStyle(kDashed),LineWidth(1)); 
   finalPDF->plotOn(frame_m2,LineColor(kBlack),LineWidth(1)); 
   data->plotOn(frame_m2,Name("data"),MarkerSize(0.5),Binning(50));
   frame_m2->Draw();
@@ -234,81 +202,25 @@ void fit_Data(){
   c1->cd(4);
   frame_m4->Draw();
 
-std::cout<<"nentries"<<tree->GetEntries()<<std::endl;	
+  std::cout<<"nentries"<<tree->GetEntries()<<std::endl;	
   c1->Draw();
-  c1->Print("massFit2.eps");
+  c1->Print("../img/massFit2.eps");
 }
 
 
-/*
-function graveyard 
+void D2hhmumuFitter::setStyle(){
 
- 
+  gStyle->SetOptStat(0);
+  gStyle->SetTitleXSize(0.05);
+  gStyle->SetTitleYSize(0.05);
+  gStyle->SetTitleFont(42,"X");
+  gStyle->SetTitleFont(42,"Y");
+  gStyle->SetLabelFont(42,"X");
+  gStyle->SetLabelFont(42,"Y");
+  gStyle->SetLabelOffset(0.01,"X");
+  gStyle->SetPadTickX(1);
+  gStyle->SetPadTickY(1);
+  TH1::SetDefaultSumw2();
+  TH2::SetDefaultSumw2();
 
-  RooRealVar mean1("mu1", "mean1", 145,144.,146.);
-  RooRealVar sigma1("sigma_{1}", "sigma1", 0.45,0.1,2.);
-  RooGaussian Gauss1("Gauss1", "Gauss1", deltaM, mean1, sigma1);
-  RooRealVar mean2("mu2", "mean2", 145.,144.,147.);
-  RooRealVar sigma2("sigma_{2}", "sigma2", 0.45,0.1,3.);
-  RooGaussian Gauss2("Gauss2", "Gauss2", deltaM, mean1, sigma2);
-  RooRealVar sigma3("sigma_{3}", "sigma3", 0.45,0.01,3.);
-  RooGaussian Gauss3("Gauss3", "Gauss3", deltaM, mean1, sigma3);
-
-  RooAddPdf PDF_deltaM_sig("PDF_deltaM_sig","signal PDF in deltaM",RooArgSet(Gauss1,Gauss2),RooArgSet(f_deltaM_sig));
-  RooRealVar f_deltaM_sig("f_sig","fraction of CB in D0 mass sigal",0.8,0.,1.);
-  RooRealVar f_deltaM_sig2("f_sig2","fraction of CB in D0 mass sigal",0.1,0.,1.);
-
-
-    //better model                                                                                                                                                                              
-
-  RooRealVar mean_mD0_sig("mean_CB_mD0_sig","mean of signal CB ",1866,1860,1880);
-  RooRealVar sigma_mD0_sig("sigma_CB_mD0_sig","mean of signal CB ",7.7,5,10);
-  RooRealVar alpha_mD0_sig1("alpha_mD0_sig1","alpha1 signal CB",2.0,1.,5.);
-  RooRealVar alpha_mD0_sig2("alpha_mD0_sig2","alpha2 signal CB",1.5,1.,5.);
-  RooRealVar n_mD0_sig1("n_mD0_sig1","n1 signal CB",1.8,0.,15.);
-  RooRealVar n_mD0_sig2("n_mD0_sig2","n2 signal CB",20.,0.,30.);
-
-  RooCBShape CB_mD0_sig1("CB_mD0_sig1","crystall ball 1 in mD0 signal PDF",D0_M,mean_mD0_sig,sigma_mD0_sig,alpha_mD0_sig1,n_mD0_sig1);
-  RooCBShape CB_mD0_sig2("CB_mD0_sig2","crystall ball 2 in mD0 signal PDF",D0_M,mean_mD0_sig,sigma_mD0_sig,alpha_mD0_sig2,n_mD0_sig2);
-
-  RooRealVar f_mD0_sig("f_sig","fraction of CB in D0 mass signal",0.8,0.,1.);
-  RooAddPdf PDF_mD0_sig("PDF_mD0_sig","signal PDF in mD0",RooArgSet(CB_mD0_sig1,CB_mD0_sig2),f_mD0_sig);
-
-
-  //delta M alternative with double crystal ball                                                                                                                                              
-
-  RooRealVar mean_deltaM_sig("mean_CB_deltaM_sig","mean of signal CB ",144,145,146);
-  RooRealVar sigma_deltaM_sig("sigma_CB_deltaM_sig","mean of signal CB ",0.1,0.1,3);
-  RooRealVar alpha_deltaM_sig1("alpha_deltaM_sig1","alpha1 signal CB",2.0,0.,5.);
-  RooRealVar alpha_deltaM_sig2("alpha_deltaM_sig2","alpha2 signal CB",1.5,0.,5.);
-  RooRealVar n_deltaM_sig1("n_deltaM_sig1","n1 signal CB",0,0.,15.);
-  RooRealVar n_deltaM_sig2("n_deltaM_sig2","n2 signal CB",0.,0.,30.);
-
-  RooCBShape CB_deltaM_sig1("CB_deltaM_sig1","crystall ball 1 in deltaM signal PDF",deltaM,mean_deltaM_sig,sigma_deltaM_sig,alpha_deltaM_sig1,n_deltaM_sig1);
-  RooCBShape CB_deltaM_sig2("CB_deltaM_sig2","crystall ball 2 in deltaM signal PDF",deltaM,mean_deltaM_sig,sigma_deltaM_sig,alpha_deltaM_sig2,n_deltaM_sig2);
-
-  //  RooRealVar f_deltaM_sig("f_sig","fraction of CB in D0 mass signal",0.8,0.,1.);                                                                                                          
-  //RooAddPdf PDF_deltaM_sig("PDF_deltaM_sig","signal PDF in deltaM",RooArgSet(CB_deltaM_sig1,CB_deltaM_sig2),f_deltaM_sig);                                                                 
-
-
-  ///peaking bkg                                                                                                                                                                              
-  RooRealVar mean_bkg("mu_{bkg}", "mean_bkg", 1840.,1820.,1860.);
-  RooRealVar sigma_bkg("sigma_{bkg}", "sigma_bkg", 28.5, 5., 70.);
-  RooGaussian bkg_Gauss("bkg_Gauss", "bkg_Gauss", D0_M, mean_bkg, sigma_bkg);
-
-  ///Exponential                                                                                                                                                                              
-  RooRealVar exp_par("lambda","exp",0.003535,-1.,1.);
-  RooExponential bkg_exp("bkg_exp","exponential background",D0_M,exp_par);
-
-  ///bkg pdf                                                                                                                                                                                  
-  RooRealVar f_bkg("f_{bkg}", "background fraction", 0.11, 0., 1.);
-  RooAddPdf bkg("bkg", "bkg", RooArgList(bkg_Gauss, bkg_exp), RooArgList(f_bkg));
-  ///total pdf                                                                                                                                                                                
-  ///----------------------                                                                                                                                                                   
-  RooRealVar n_sig("n_sig", "n_sig", data->numEntries()/2., 0., data->numEntries());
-  RooRealVar n_bkg("n_bkg", "n_bkg", data->numEntries()/2. , 0., data->numEntries());
-
-  RooAbsPdf* pdf=new RooAddPdf("pdf", "pdf", RooArgList(Gauss1,bkg), RooArgList(n_sig,n_bkg));
-
- 
-  */
+}
--- dev/src/D2hhmumuFitter.h
+++ dev/src/D2hhmumuFitter.h
@@ -50,10 +50,75 @@
 #include "RooThreshold.h"
 #include "D2hhmumuModel.h"
 
-void fit_MC();
-void fit_Data();
+class D2hhmumuFitter {
 
+ public:
+ D2hhmumuFitter() ;
+  
+  ~D2hhmumuFitter();
 
+  //variables used in the fitter
+
+  RooRealVar D0_M;
+  RooRealVar deltaM;
+  
+  //signal
+  RooRealVar deltaM_xi;
+  RooRealVar deltaM_lambda;
+  RooRealVar deltaM_gamma;
+  RooRealVar deltaM_delta;
+
+  RooRealVar D0_M_xi;
+  RooRealVar D0_M_lambda;
+  RooRealVar D0_M_gamma;
+  RooRealVar D0_M_delta;
+
+  //purely combinatorial background                                                                                                                                             
+  RooRealVar deltaM_threshold;
+  RooRealVar deltaM_alpha;
+
+  RooRealVar D0_M_chebyA;
+  RooRealVar D0_M_chebyB;
+  RooRealVar D0_M_chebyC;
+ 
+  RooRealVar mean1;
+  RooRealVar sigma1;
+   
+  /*
+  RooRealVar D0_M("Dst_DTF_D0_M", "m(h h #mu #mu)", 1800., 1940.,"MeV");
+  RooRealVar deltaM("deltaM","#delta m", 139.8,150,"MeV");
+  
+  //signal
+  RooRealVar deltaM_xi("deltaM_xi","deltaM_xi",1.45437e+02,144,146);
+  RooRealVar deltaM_lambda("deltaM_lambda","deltaM_lambda",5.35039e-01,0.1,2);
+  RooRealVar deltaM_gamma("deltaM_gamma","deltaM_gamma",1.00164e-01,-2,2);
+  RooRealVar deltaM_delta("deltaM_delta","deltaM_delta",1.07829e+00,0.,10);
+
+  RooRealVar D0_M_xi("D0_M_xi","D0_M_xi",1.86560e+03,1860,1870);
+  RooRealVar D0_M_lambda("D0_M_lambda","D0_M_lambda",8.54839e+00,0.1,20);
+  RooRealVar D0_M_gamma("D0_M_gamma","D0_M_gamma",-5.42758e-02,-2,2);
+  RooRealVar D0_M_delta("D0_M_delta","D0_M_delta",6.09288e-01,0.,10);
+
+  //purely combinatorial background                                                                                                                                             
+  RooRealVar deltaM_threshold("deltaM_threshold","deltaM_threshold",139.57018);
+  RooRealVar deltaM_alpha("deltaM_alpha","deltaM_alpha",3.9761e+00,0,10.);
+
+  RooRealVar D0_M_chebyA("D0_M_chebyA","D0_M_chebyA",-3.5906e-02,-1,1);
+  RooRealVar D0_M_chebyB("D0_M_chebyB","D0_M_chebyB",-1.7004e-02,-1,1);
+  RooRealVar D0_M_chebyC("D0_M_chebyC","D0_M_chebyC",-1.7882e-02,-1,1);
+ 
+  //peaking , in mD0 just a Gaussian... to be improved!                                                                                                                         
+  RooRealVar mean1("mu1", "mean1", 1.8350e3,1835.,1845.);
+  RooRealVar sigma1("sigma_{1}", "sigma1", 1.4187e+01,3.,25.);
+  */
+  
+  void setKpimumuStartParameters(); //to be implemented, especially when there will be other channles..
+  
+  void setStyle();
+  void fit_MC();
+  void fit_Data();
+
+};
  
 
   
